7c7f6c3b1f485c7099842849ae898fb108f391d3
hardik yadav <hardikyadav1182005@gmail.com>
2023-09-13 20:53:05 +0530

    'Ewn

---
+++ dsa probs/reversing using stack.cpp
@@ -0,0 +1,30 @@
+// homework 
+#include<iostream>
+#include<string>
+#include<stack>
+using namespace std;
+void reverse_s(string s)
+{
+	stack<string> st;
+for(int i=0; i<s.length();i++){
+	string word="";
+	while(s[i]!=' '&& i<s.length()){
+		word=word+s[i];
+		i++;	
+	}
+	st.push(word);
+	
+}
+while(!st.empty()){
+	cout<<st.top()<<" ";
+	st.pop();
+	
+	
+}
+cout<<endl;
+
+}
+int main(){
+	string s= "hello, how are you";
+	reverse_s(s);
+}
